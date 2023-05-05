33c9233141ee77f10740ba30612c6a7df0be1e16
Andres Suarez <andresfelipe9619@gmail.com>
2023-05-05 06:37:20 -0500

    :art: Format files

--- src/pages/clients/Confidentially.jsx
+++ src/pages/clients/Confidentially.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Grid, Typography } from "@mui/material";
 
 const styled = {
-    text: {
-        backgroundColor: "red"
-    }
-}
+  text: {
+    backgroundColor: "red",
+  },
+};
 
-const Confidentially = () =>{
-    return(
-        <div>
-            <Typography sx={styled.text} variant="h1">prueba</Typography>
-        </div>
-    )
-}
+const Confidentially = () => {
+  return (
+    <div>
+      <Typography sx={styled.text} variant="h1">
+        prueba
+      </Typography>
+    </div>
+  );
+};
 
 export default Confidentially;
