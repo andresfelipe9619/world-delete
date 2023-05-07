2a74191710946f9b738c8e8bb8dc0b5347b6f12d
Andres Suarez <andresfelipe9619@gmail.com>
2023-05-07 12:44:26 -0500

    :sparkles: Complete contact us page

--- src/pages/contact/ContactUs.js
+++ src/pages/contact/ContactUs.js
@@ -6,22 +6,40 @@ import leftMap from "../../assets/contactus/left-map.png";
 import rightMap from "../../assets/contactus/right-map.png";
 const ContactUs = () => {
   return (
-    <Grid container sx={{ py: 3 }} spacing={4}>
-      <Grid item xs={12} container>
-        <Typography variant={"h2"}>Who we are?</Typography>
-        <Typography>Miami</Typography>
+    <Grid container sx={{ py: 3 }} spacing={2}>
+      <Grid item xs={12} container spacing={2}>
+        <Grid item xs={3}>
+          <Typography variant={"h1"} component={"h2"}>
+            Where we are?
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography align={"right"} variant={"h3"}>
+            Miami
+          </Typography>
+        </Grid>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <img alt={"left-map"} src={leftMap} />
+        <img alt={"left-map"} src={leftMap} height={"80%"} width={"100%"} />
       </Grid>
-      <Grid item xs={12} sm={6} container sx={{ pt: 4 }}>
-        <Grid item sm={6}>
-          <Typography variant={"h2"}>Lets talk</Typography>
+      <Grid item xs={12} sm={6} container sx={{ my: 8 }}>
+        <Grid item sm={4} container justifyContent={"center"}>
+          <Typography variant={"h1"} component={"h2"}>
+            Lets talk
+          </Typography>
         </Grid>
-        <Grid item sm={6} container direction={"column"}>
-          <Typography>+1 (918) 309-2025</Typography>
-          <Typography>info@world-delete.com</Typography>
-          <Typography>4300 Biscayne Blvd, Miami, FL 33137, EE.UU</Typography>
+        <Grid
+          item
+          sm={8}
+          container
+          direction={"column"}
+          alignContent={"center"}
+        >
+          <Typography paragraph>+1 (918) 309-2025</Typography>
+          <Typography paragraph>info@world-delete.com</Typography>
+          <Typography paragraph>
+            4300 Biscayne Blvd, Miami, FL 33137, EE.UU
+          </Typography>
         </Grid>
         <img alt={"left-map"} src={rightMap} />
       </Grid>
