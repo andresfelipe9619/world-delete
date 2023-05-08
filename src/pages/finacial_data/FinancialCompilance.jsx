3e384d58d0117116828b93524e50d0a4dfde09af
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-05-07 20:04:59 -0500

    add buttons to finance data conficidality

--- src/pages/finacial_data/FinancialCompilance.jsx
+++ src/pages/finacial_data/FinancialCompilance.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FormTriangle from "../home/FormTriangle";
 import { Grid, Typography, Button } from "@mui/material";
 import BannerText from "../googleInformation/BannerText";
+import styles from "../googleInformation/styles";
 
 /**
  * @AUTHOR <cristian.machado@correounivalle.edu.co>
@@ -16,7 +17,31 @@ const FinancialCompilance = (props) => {
       <BannerText 
             url={props.url} 
             content={(props.json.FinancialCompilance)} 
-      />
+      >
+                <ol className={`ol_style_container`}>
+                <li>
+                    <Typography  sx={styles.textNormal}>
+                        Currently there are three well-known and frequently-
+                    </Typography>
+                    <Typography  sx={styles.textNormal}>
+                        used databases to which only the credit institutions,
+                    </Typography>
+                    <Typography  sx={styles.textNormal}>
+                        corporations and private entities have access
+                    </Typography>
+                    <div style={{
+                       marginTop: "1em"
+                    }}>
+                      <Button variant={"contained"}>
+                          Contact us
+                      </Button>
+                      <Button sx={{ ml: 4 }}>
+                         Completely confidential call
+                      </Button>
+                    </div>
+                </li>
+          </ol>
+      </BannerText>
       <FormTriangle />
     </>
   );
